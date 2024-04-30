a2740026efebfd4edb42430bf17a8085e3d4e9a2
Barry Brown <profbbrown@gmail.com>
2024-04-29 20:56:30 -0700

    Set the render function automatically

--- asobi/__init__.py
+++ asobi/__init__.py
@@ -1,12 +1,17 @@
+import sys
+import inspect
 import pygame
 
 __all__ = ['fill_screen', 'set_renderer']
 
-pygame.init()
-_screen = pygame.display.set_mode((640, 480))
-_clock = pygame.time.Clock()
-_renderer = None
 
+def _get_renderer():
+    main_module = sys.modules['__main__']
+    r = getattr(main_module, 'render', None)
+    if callable(r):
+        return r
+    else:
+        return None
 
 def fill_screen(color):
     _screen.fill(color)
@@ -15,11 +20,25 @@ def set_renderer(fn):
     global _renderer
     _renderer = fn
 
-while True:
+pygame.init()
+_screen = pygame.display.set_mode((640, 480))
+_clock = pygame.time.Clock()
+_renderer = _get_renderer()
+
+_running = True
+
+while _running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            _running = False
 
-    _renderer()
+    if callable(_renderer):
+        _renderer()
+    else:
+        print("No render function was found.")
+        break
+
     pygame.display.flip()
     _clock.tick(20)
+
+pygame.quit()
